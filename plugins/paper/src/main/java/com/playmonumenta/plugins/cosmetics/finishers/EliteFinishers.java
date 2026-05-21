c60355c6929ea7d0f252aa372ae2fcfd6436ddbb
Timothy Southwick <NickNackGus@gmail.com>
2026-05-20 23:12:29 -0400

    Added Anniversary Finisher (#6108)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/cosmetics/finishers/EliteFinishers.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/cosmetics/finishers/EliteFinishers.java
@@ -29,6 +29,7 @@ public class EliteFinishers {
 	private static final ImmutableMap<String, EliteFinisher> FINISHERS =
 		ImmutableMap.<String, EliteFinisher>builder()
 			.put(AdvancedAudioMechanism.NAME, new AdvancedAudioMechanism())
+			.put(AnniversaryFinisher.NAME, new AnniversaryFinisher())
 			.put(BirthdayThemeFinisher.NAME, new BirthdayThemeFinisher())
 			.put(BlastOffFinisher.NAME, new BlastOffFinisher())
 			.put(BuzzedFinisher.NAME, new BuzzedFinisher())
