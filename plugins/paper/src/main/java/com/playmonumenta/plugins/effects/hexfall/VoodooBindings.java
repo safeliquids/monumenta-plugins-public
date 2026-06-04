a17ce8b38fcc36391be97f91376052f4130085c0
LucyChroma <117107593+LucyChroma@users.noreply.github.com>
2026-06-03 21:27:25 -0400

    Change circle/donut icons for Voodoo Bindings (#6444)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/effects/hexfall/VoodooBindings.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/effects/hexfall/VoodooBindings.java
@@ -33,8 +33,8 @@ public class VoodooBindings extends Effect {
 	private final BossBar mBossBar;
 	private @Nullable TextDisplay mDisplay;
 	public final Queue<VoodooBinding> mBindings;
-	public static final String CIRCLE = "⦿";
-	public static final String DONUT = "⦾";
+	public static final String CIRCLE = "⏺";
+	public static final String DONUT = "⭘";
 
 	public enum VoodooBinding {
 		WHITE_CIRCLE(0, NamedTextColor.WHITE, CIRCLE, "conform", "WC"),
